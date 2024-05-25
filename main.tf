26a23b5f44ac9e826b7f0ab55e7e0499b57914cb
Andrii Kovalchuk <andriy.p.kovalchuk@gmail.com>
2024-05-25 16:22:58 +0300

    Jenkinsfile devided to three files

--- main.tf
+++ main.tf
@@ -1,34 +1,5 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2204-jammy-v20240519"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
-}
-
-# resource "google_compute_network" "vpc_network" {
-#   name                    = "terraform-network"
-#   auto_create_subnetworks = "true"
-# }
-
-resource "google_sql_database_instance" "main" {
-  name                = "main-instance"
-  database_version    = "POSTGRES_14"
-  deletion_protection = false
-
-  settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
-    tier = "db-f1-micro"
-  }
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
 }
--- provider.tf
+++ provider.tf
@@ -1,15 +1,26 @@
+# terraform {
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "5.30.0"
+#     }
+#   }
+# }
+
+# provider "google" {
+#   project = "amplified-grail-423921-n7"
+#   region  = "europe-north1"
+#   zone    = "europe-north1-c"
+# }
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.30.0"
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.2"
     }
   }
 }
 
-provider "google" {
-  project = "amplified-grail-423921-n7"
-  region  = "europe-north1"
-  zone    = "europe-north1-c"
+provider "random" {
+  # Configuration options
 }
-
